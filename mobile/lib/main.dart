8478580249fac9967f798fb9d4b520b83478956c
aserhatsahin <aserhatsahin@gmail.com>
2025-12-11 19:48:46 +0300

    [setup & initializattion] Firebase setup for IOS and Android
    [setup & initialization]
    firebase setup for ios and android

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+import 'firebase_options.dart';
 import 'core/theme/app_colors.dart';
 import 'features/auth/presentation/screens/register_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  print("🔥 Firebase initialized apps: ${Firebase.apps}");
   runApp(const VacanzaApp());
 }
 
@@ -11,6 +20,7 @@ class VacanzaApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
     return MaterialApp(
       title: 'Vacanza',
       debugShowCheckedModeBanner: false,
@@ -20,7 +30,7 @@ class VacanzaApp extends StatelessWidget {
           seedColor: AppColors.primary,
           brightness: Brightness.light,
         ),
-        fontFamily: 'SF Pro', // yoksa silebilirsin
+        fontFamily: 'SF Pro', // varsa kalsın, yoksa da sorun değil
       ),
       home: const RegisterScreen(),
     );
