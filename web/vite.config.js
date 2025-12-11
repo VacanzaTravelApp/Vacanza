5dfd2c4ab37bc1d06cb79be5853eddd83aea4956
Ömer Faruk Asil <asilo02@hotmail.com>
2025-12-12 01:38:57 +0300

    VACANZA-155
    Mapbox için sıkıntı oluşturan harita gözükmeme olayı çözüldü.

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,25 +1,16 @@
-// vite.config.js (CSS hatasını da çözen versiyon)
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  
-  // 🔥 KRİTİK AYARLAR:
   optimizeDeps: {
-    exclude: ['react-map-gl', 'mapbox-gl'],
+    include: ['mapbox-gl'], // exclude yerine include kullanın
   },
-
   resolve: {
     alias: {
       'mapbox-gl': 'mapbox-gl/dist/mapbox-gl.js',
-      // CSS dosyasını manuel olarak node_modules içindeki yerine yönlendirir.
-      // Not: 'path' modülünün yüklenmesi gerekir.
-      // 'mapbox-gl/dist/mapbox-gl.css': path.resolve(__dirname, 'node_modules/mapbox-gl/dist/mapbox-gl.css') 
     },
   },
-  
   define: {
     'process.env': {} 
   }
