180596dc43582aaa69b7bd9becf82a57036bed56
UfukZencirci <Ufuk.zencirci@ug.bilkent.edu.tr>
2025-12-13 16:18:05 +0300

    Update UserInfoController.java

--- backend/src/main/java/com/vacanza/backend/controller/UserInfoController.java
+++ backend/src/main/java/com/vacanza/backend/controller/UserInfoController.java
@@ -1,10 +1,13 @@
 package com.vacanza.backend.controller;
 
 
+import com.vacanza.backend.dto.request.UserInfoRequestDTO;
+import com.vacanza.backend.dto.response.UserInfoResponseDTO;
 import com.vacanza.backend.service.UserInfoService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/userInfo")
@@ -12,5 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserInfoController {
 
     private final UserInfoService userInfoService;
-    
+
+    @GetMapping("/get-profile")
+    public ResponseEntity<UserInfoResponseDTO> getUserInfo(){
+        UserInfoResponseDTO response = userInfoService.getUserInfo();
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PostMapping("/update-profile")
+    public ResponseEntity<UserInfoResponseDTO> updateUserInfo(@RequestBody UserInfoRequestDTO request) {
+        UserInfoResponseDTO response = userInfoService.updateUserInfo(request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
