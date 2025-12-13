1204e91a06dd80966e9a635cf3175063be5bdc0a
UfukZencirci <Ufuk.zencirci@ug.bilkent.edu.tr>
2025-12-13 15:14:16 +0300

    Update UserInfoController.java

--- backend/src/main/java/com/vacanza/backend/controller/UserInfoController.java
+++ backend/src/main/java/com/vacanza/backend/controller/UserInfoController.java
@@ -1,4 +1,12 @@
 package com.vacanza.backend.controller;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/userInfo")
 public class UserInfoController {
+
+    
 }
