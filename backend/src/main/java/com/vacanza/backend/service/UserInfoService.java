dfd291d41a7d780344ac09a8a82bd4698fb7e8c1
UfukZencirci <Ufuk.zencirci@ug.bilkent.edu.tr>
2025-12-13 13:15:44 +0300

    Update UserInfoService.java

--- backend/src/main/java/com/vacanza/backend/service/UserInfoService.java
+++ backend/src/main/java/com/vacanza/backend/service/UserInfoService.java
@@ -7,10 +7,11 @@ import com.vacanza.backend.entity.UserInfo;
 import com.vacanza.backend.exceptions.enums.UserExceptionEnum;
 import com.vacanza.backend.repo.UserInfoRepository;
 import com.vacanza.backend.repo.UserRepository;
-import com.vacanza.backend.service.impl.UserInfoImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import com.vacanza.backend.service.impl.UserInfoImpl;
 
 import java.util.UUID;
 
@@ -21,6 +22,19 @@ public class UserInfoService implements UserInfoImpl {
     private final UserInfoRepository userInfoRepository;
     private final UserRepository userRepository;
     private final UserService userService;
+
+    public UserInfoResponseDTO getUserInfo() {
+
+        // 1. Get the string UID again
+        String firebaseUid = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        // 2. Find the User Entity
+        User currentUser = userRepository.findByFirebaseUid(firebaseUid)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+    }
+
+
 
 
 
