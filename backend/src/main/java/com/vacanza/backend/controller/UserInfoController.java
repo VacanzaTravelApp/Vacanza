149d7bf238e2ec3a9d8c6a26c1bf8ece1d64837b
UfukZencirci <Ufuk.zencirci@ug.bilkent.edu.tr>
2025-12-13 15:21:50 +0300

    Update UserInfoController.java

--- backend/src/main/java/com/vacanza/backend/controller/UserInfoController.java
+++ backend/src/main/java/com/vacanza/backend/controller/UserInfoController.java
@@ -1,12 +1,16 @@
 package com.vacanza.backend.controller;
 
 
+import com.vacanza.backend.service.UserInfoService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/userInfo")
+@AllArgsConstructor
 public class UserInfoController {
 
+    private final UserInfoService userInfoService;
     
 }
