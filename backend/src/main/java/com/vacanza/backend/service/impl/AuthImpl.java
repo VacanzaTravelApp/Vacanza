1d1858534dc1ff795edaf2dabfef70617c384085
Goksu Uzun <120195166+gghinzu@users.noreply.github.com>
2025-12-14 18:10:06 +0300

    name fixes

--- backend/src/main/java/com/vacanza/backend/service/impl/AuthImpl.java
+++ backend/src/main/java/com/vacanza/backend/service/impl/AuthImpl.java
@@ -1,11 +1,9 @@
 package com.vacanza.backend.service.impl;
 
-import com.vacanza.backend.dto.request.UserRegisterRequestDTO;
 import com.vacanza.backend.dto.response.UserAuthenticationDTO;
-import com.vacanza.backend.dto.response.UserRegisterResponseDTO;
 import jakarta.servlet.http.HttpServletRequest;
 
 public interface AuthImpl {
     UserAuthenticationDTO getMe(HttpServletRequest request);
-    UserRegisterResponseDTO onboard(UserRegisterRequestDTO req);
+    //UserRegisterResponseDTO onboard(UserRegisterRequestDTO req);
 }
