ab03aeadb16d733bda4064bf467ce5d1b17d2eb9
aserhatsahin <aserhatsahin@gmail.com>
2025-12-26 15:03:48 +0300

    Map ekranı sadece MapBloc provider + action dispatch yapacak şekilde netleştirildi.
    Map ekranı sadece MapBloc provider + action dispatch yapacak şekilde netleştirildi.

--- mobile/lib/features/map/presentation/screens/home_map_screen.dart
+++ mobile/lib/features/map/presentation/screens/home_map_screen.dart
@@ -1,28 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../poi_search/presentation/bloc/area_query_bloc.dart';
 import '../bloc/map_bloc.dart';
 import '../bloc/map_event.dart';
 import '../bloc/map_state.dart';
 import '../widgets/home_map/home_map_scaffold.dart';
 
 /// Login sonrası kullanıcıyı karşılayan ana harita ekranı.
-/// Task 138: Toggle/Recenter aksiyonları BLoC'a dispatch eder.
+/// - MapBloc: 2D/3D + recenter
+/// - AreaQueryBloc: viewport bbox üretme (VACANZA-200)
 class HomeMapScreen extends StatelessWidget {
   const HomeMapScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<MapBloc>(
-      // Bu ekran açılınca MapBloc oluşturulur.
-      create: (_) => MapBloc(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<MapBloc>(
+          create: (_) => MapBloc(),
+        ),
+        BlocProvider<AreaQueryBloc>(
+          create: (_) => AreaQueryBloc(),
+        ),
+      ],
       child: const _HomeMapView(),
     );
   }
 }
 
-/// Provider'dan gelen MapBloc state'ini dinleyen gerçek UI.
-/// (Provider ile UI'yi ayırıyoruz ki test/okunabilirlik iyi olsun.)
+/// Provider'lardan gelen state'leri dinleyen gerçek UI.
 class _HomeMapView extends StatelessWidget {
   const _HomeMapView();
 
@@ -32,9 +39,6 @@ class _HomeMapView extends StatelessWidget {
       builder: (context, state) {
         return HomeMapScaffold(
           mode: state.viewMode,
-        //  onOpenMapStyle: () {
-            // Task 138 kapsamı değil; şimdilik boş bırak.
-         // },
           onToggleMode: () {
             context.read<MapBloc>().add(ToggleViewModePressed());
           },
