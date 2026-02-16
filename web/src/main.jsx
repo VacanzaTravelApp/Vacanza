592253e9e8379f08bdc30c8101e617b6b8e08c00
Damla Alçay <alcaydamla2@gmail.com>
2026-02-16 20:38:48 +0300

    VACANZA-251
    POI fix and Loading/Error UI

--- web/src/main.jsx
+++ web/src/main.jsx
@@ -6,8 +6,6 @@ import "./index.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import AuthProvider from "./context/AuthProvider";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
